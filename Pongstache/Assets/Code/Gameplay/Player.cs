f4aa8930be64e7b92dcb20347c7ddcb2cd3c86bb
Oskar <kogutoskar123@gmail.com>
2019-10-03 20:48:25 +0200

    Added player movement

---
+++ Pongstache/Assets/Code/Gameplay/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Gameplay
+{
+    /// <summary>
+    /// Player settings
+    /// </summary>
+    public class Player : MonoBehaviour
+    {
+        [SerializeField]
+        private PlayerSO _playerSettings;
+
+        public float Speed { get => _playerSettings.Speed; }
+        public float Clamp { get => _playerSettings.Clamp; }
+    }
+}
---
+++ Pongstache/Assets/Code/Gameplay/PlayerSO.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Gameplay
+{
+    [CreateAssetMenu(fileName = "PlayerSettings", menuName = "Gameplay/Player")]
+    public class PlayerSO : ScriptableObject
+    {
+        [SerializeField]
+        [Range(0.1f, 10f)]
+        private float _playerSpeed;
+
+        [SerializeField]
+        private float _playerClamp;
+
+        public float Speed { get => _playerSpeed; }
+        public float Clamp { get => _playerClamp; }
+    }
+}
